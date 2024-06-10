a932f2f23096fd9b64551873f60bb01057e51ede
Joan <joanes.catalan@gmail.com>
2024-06-10 12:42:05 -0300

    Hecho Ruteo

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,17 @@ import Page from "./components/UI/page";
 import Login from './login'
 
 function App() {
-
-  //Accounts
-  //Login
-  //FixedTermDeposit
-  //LoanController
-  //TransactionController
-  //UserController
-
-
   return (
     <Page>
       <Routes>
-
         
         <Route path="/accounts" element={""}/>
-        <Route path="/accounts/create" element={""}/>
-        <Route path="/accounts/editar/:accountId" element={""}/>
         <Route path="/accounts/:userId" element={""}/>
-        <Route path="/accounts/select/:accountId" element={""}/>
         <Route path="/accounts/balance" element={""}/>
 
-        <Route path="/auth/login" element={""}/>
-        <Route path="/auth/register" element={""}/>
-        <Route path="/auth/register/admin" element={""}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signUp" element={""}/>
+        <Route path="/signUp/admin" element={""}/>
 
         <Route path="/fixedTerm" element={""}/>
         <Route path="/fixedTerm/simulate" element={""}/>
@@ -35,23 +22,11 @@ function App() {
         <Route path="/loan/simulate" element={""}/>
 
         <Route path="/transactions" element={""}/>
-        <Route path="/transactions/sendArs" element={""}/>
-        <Route path="/transactions/sendUsd" element={""}/>
-        <Route path="/transactions/payment" element={""}/>
-        <Route path="/transactions/deposit" element={""}/>
         <Route path="/transactions/user/:userId" element={""}/>
-        <Route path="/transactions/transaction/:transactionId" element={""}/>
-        <Route path="/transactions/detail/:id" element={""}/>
+        <Route path="/transactions/:transactionId" element={""}/>
 
         <Route path="/users" element={""}/>
-        <Route path="/id/:id" element={""}/>
         <Route path="/detail/:id" element={""}/>
-        <Route path="/cbu/:idCbu/users/:idUser" element={""}/>
-        <Route path="/users/:id" element={""}/>
-
-
-        <Route path="/auth/login" element={<Login/>}/>
-
 
       </Routes>
     </Page>
