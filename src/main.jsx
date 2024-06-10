5da3415bb0d84682c5f6e0a086ad38467a56a86e
Msoteras <msoteras@frba.utn.edu.ar>
2024-06-10 12:49:58 -0300

    Integro login con redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import store, {persistor} from ".components/Redux/store.jsx" //chequear
+import store, {persistor} from './Redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
