9ef573eb2017d761bb3350c76e9fd098e0462aee
Joan <joanes.catalan@gmail.com>
2024-06-12 19:42:42 -0300

    Listo primera base con Css

--- src/components/PlazoFijoSimulado/index.jsx
+++ src/components/PlazoFijoSimulado/index.jsx
@@ -1,23 +1,23 @@
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import Button from "@mui/material/Button";
-import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
 import MenuItem from "@mui/material/MenuItem";
-import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { useState } from "react";
+import "./fixedTerm.css";
+
 export default function PlazoFijoSimulado() {
-  const [dias, setDias] = useState("");
+  const [dias, setDias] = useState(30);
 
   const handleChange = (event) => {
     setDias(event.target.value);
   };
 
   return (
-    <>
+    <section className="box-principal">
       <p>
         <br />
       </p>
@@ -27,40 +27,50 @@ export default function PlazoFijoSimulado() {
         component="form"
         sx={{
           "& .MuiTextField-root": { m: 1, width: "25ch" },
+          "& .MuiInputBase-input": { color: "#ffffff" },
+          "& .MuiInputLabel-root": { color: "#ffffff" },
+          "& .MuiSelect-root": { color: "#ffffff" },
+          "& .MuiSelect-icon": { color: "#ffffff" },
+          "& .MuiInputAdornment-root": { color: "#ffffff" },
         }}
         noValidate
         autoComplete="off"
+        id="box-secundario"
       >
         <div>
           <Typography>Monto a invertir</Typography>
-          <TextField required id="outlined-required" defaultValue="$200.000" />
+          <TextField
+            required
+            id="outlined-required"
+            className="white-text-field"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start" 
+                >
+                  $
+                </InputAdornment>
+              ),
+            }}
+          />
         </div>
         <div>
           <Typography>Días a invertir</Typography>
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <Select
-              id="demo-simple-select-helper"
-              value={dias}
-              onChange={handleChange}
-            >
-              <MenuItem value="">
-              </MenuItem>
+          <FormControl sx={{ m: 1, minWidth: 120 }} className="white-select">
+            <Select value={dias} onChange={handleChange}>
               <MenuItem value={30}>30 días</MenuItem>
               <MenuItem value={90}>90 días</MenuItem>
               <MenuItem value={180}>180 días</MenuItem>
             </Select>
-            <FormHelperText>With label + helper text</FormHelperText>
           </FormControl>
         </div>
         <div>
           <Button variant="outlined">Simular Plazo Fijo</Button>
         </div>
-       
       </Box>
       <Typography>
         Si la fecha de vencimiento cae un dia no habill, la misma se pasará al
         primer dia hábil del siguiente.
       </Typography>
-    </>
+    </section>
   );
 }
