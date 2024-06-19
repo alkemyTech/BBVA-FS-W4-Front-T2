2fa43d8207ab09a3837809db1ba1da4fec5fd9eb
Msoteras <msoteras@frba.utn.edu.ar>
2024-06-19 11:51:27 -0300

    modifico footer

--- src/UI/Footer/index.jsx
+++ src/UI/Footer/index.jsx
@@ -1,50 +1,41 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import PhoneIcon from '@mui/icons-material/Phone';
-import '/src/index.css';
+import './footer.css'; // Importa el archivo CSS de estilos
 
 function Pie() {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', py: 1, px: 2}}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <IconButton href="https://facebook.com" sx={{ color: '#fff' }}>
+    <footer className="footer">
+      <div className="social-icons">
+        <IconButton href="https://facebook.com">
           <FacebookIcon />
         </IconButton>
-        <IconButton href="https://twitter.com" sx={{ color: '#fff' }}>
+        <IconButton href="https://twitter.com">
           <TwitterIcon />
         </IconButton>
-        <IconButton href="https://linkedin.com" sx={{ color: '#fff' }}>
+        <IconButton href="https://linkedin.com">
           <LinkedInIcon />
         </IconButton>
-      </Box>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <IconButton href="mailto:gatosmonteses@bbva.com" sx={{ p: 0, fontSize: 'inherit', mr: 2, color: '#fff' }}>
+      </div>
+      <div className="contact-info">
+        <a href="mailto:gatosmonteses@bbva.com" className="contact-item">
           <EmailIcon sx={{ verticalAlign: 'middle', mr: 1, fontSize: '18px' }} />
-          <Typography variant="body2" sx={{ fontSize: '12px' }}>
-            gatosmonteses@bbva.com
-          </Typography>
-        </IconButton>
-        <IconButton href="tel:+123456789" sx={{ p: 0, fontSize: 'inherit', color: '#fff' }}>
+          <span>gatosmonteses@bbva.com</span>
+        </a>
+        <a href="tel:+123456789" className="contact-item">
           <PhoneIcon sx={{ verticalAlign: 'middle', mr: 1, fontSize: '18px' }} />
-          <Typography variant="body2" sx={{ fontSize: '12px' }}>
-            +123 456 789
-          </Typography>
-        </IconButton>
-      </Box>
-      <Box sx={{ textAlign: 'center', color: '#fff' }}>
-        <Typography variant="body2" sx={{ fontSize: '12px' }}>
-          &copy; {new Date().getFullYear()} By Gatos Monteses
-        </Typography>
-      </Box>
-    </Box>
+          <span>+123 456 789</span>
+        </a>
+      </div>
+      <div className="copyright">
+        <p>&copy; {new Date().getFullYear()} By Gatos Monteses</p>
+      </div>
+    </footer>
   );
 }
 
 export default Pie;
-
