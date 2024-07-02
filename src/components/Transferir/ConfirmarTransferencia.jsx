7962e565211483fcfc6918feafbff84ef8586620
joanktalan <joanes.catalan@gmail.com>
2024-07-02 02:17:06 -0300

    Modificadas 2 vistas

--- src/components/Transferir/ConfirmarTransferencia.jsx
+++ src/components/Transferir/ConfirmarTransferencia.jsx
@@ -5,22 +5,12 @@ import "./transferir.css"
 const ConfirmarTransferencia = ({ transferData,  handleBack, handleConfirm, isLoading }) => {
   return (
     <Box display={"flex"} justifyContent={"center"} flexDirection={"column"} textAlign={"center"}> 
-      <Box border={"1px solid #1565c0"} padding={2} textAlign={'left'}>
-        <Typography gutterBottom>
+      <Box border={"1px solid #1565c0"} padding={2}  fontSize={20} textAlign={'center'} className="box-datos-transferencia">
           <b>CBU Destino:</b> {transferData.cbu}
-        </Typography>
-        <Typography gutterBottom>
           <b>Destinatario: </b> {transferData.accountInfo.firstName} {transferData.accountInfo.lastName}
-        </Typography>
-        <Typography gutterBottom>
           <b>Cuenta: </b>  {transferData.accountInfo.accountType}
-        </Typography>
-        <Typography gutterBottom>
           <b>Alias: </b> {transferData.accountInfo.alias}
-        </Typography>
-        <Typography gutterBottom>
           <b>Monto:</b> ${transferData.amount}
-        </Typography>
       </Box>
 
       <Box
